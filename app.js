81414ba7e4115805cff4a99c41878f6baada2341
Christopher-Dev2099 <cristhoper@moneibox.com>
2024-05-21 22:40:26 -0500

    fix: deatils in app.js fixes

---
+++ app.js
@@ -0,0 +1,14 @@
+// Taller Final Unit Testing
+
+const capitalizeText = require("./src/capitalize_text_strings/capitalizeText");
+const factorial = require("./src/factorial_of_a_number/factorialNumber");
+const numberPrime = require("./src/number_is_prime/numberPrime");
+
+console.log("Hello world!");
+
+console.log(factorial(5));
+
+console.log(numberPrime(97));
+
+console.log(capitalizeText("chanchito feliz"));
+
